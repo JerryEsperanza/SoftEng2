0ad2ca449478a61415002de15ade73c92b14e5cb
Jeremias Esperanza <142370600+JerryEsperanza@users.noreply.github.com>
2024-01-31 23:59:24 +0000

    Pending changes exported from your codespace

---
+++ Car.java
@@ -0,0 +1,7 @@
+public class Car{
+    private final String brand;
+    private final String engineType;
+    private final String transmission;
+    private final String color;
+    private final Integer capacity;
+}
